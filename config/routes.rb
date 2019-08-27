e895a625bb504e026a13b729ed71b729e1d7a3cd
pxyco <andvalderrama@gmail.com>
2019-08-27 05:43:39 +0000

    Heroku: change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root to:  'static_pages#home'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
   resources :pages
-  root to:  'static_pages#home'
-  get       '/share',    to: 'pages#show'
-  # post      '/share',    to: 'pages#create'
   
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get       '/share',    to: 'pages#show'
+  
 end
