885e93b525ee25f516bff5dc72f98fca80962c0b
pxyco <andvalderrama@gmail.com>
2019-08-28 03:38:56 +0000

    Fixed graph method with ruby

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,17 +1,16 @@
 module PagesHelper
   def calories_char_data
     start_date = 1.month.ago
-    calories_earned_by_day = current_user.posts.total_grouped_by_day(start_date)
+    calories_earned_by_day = current_user.posts.grouped_sumed_by_day(start_date)
     
-      
+    puts   calories_earned_by_day
     (start_date.to_date..Date.today).map do |date|
       {
           created_at: date,
-          calories_earned: calories_earned_by_day[date].try(:first) && 
-                            calories_earned_by_day[date].first.try(:total_calories_earned) || 0,
-          calories_lost: calories_earned_by_day[date].try(:first) && 
-                            calories_earned_by_day[date].first.try(:total_calories_lost) || 0
+          calories_earned: calories_earned_by_day[date].try(:[], :calories_earned) || 0,
+          calories_lost: calories_earned_by_day[date].try(:[], :calories_lost) || 0,
       }
     end
   end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,19 @@ class Post < ApplicationRecord
                                 sum(calories_lost) as total_calories_lost")
     calories = calories.group_by { |c| c.created_at.to_date }
   end
+  
+  def self.grouped_sumed_by_day(start)
+    calories = where(created_at: start.beginning_of_day..Time.zone.now)
+    data_calories = Hash.new
+    calories.each do |cal|
+      date = cal.created_at.to_date
+      data_calories[date] = Hash.new if data_calories[date].nil?
+      data_calories[date][:calories_earned] = 0 if data_calories[date][:calories_earned].nil? 
+      data_calories[date][:calories_lost] = 0 if data_calories[date][:calories_lost].nil? 
+      data_calories[date][:calories_earned] += cal.calories_earned
+      data_calories[date][:calories_lost] += cal.calories_lost
+    end
+    return data_calories
+  end
     
 end
